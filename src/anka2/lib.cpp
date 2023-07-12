db40977f9e8256a4d4e595097c3099a60666e6b6
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2023-07-12 22:42:53 +0200

    add basic project with llvm and mlir connection

---
+++ src/anka2/lib.cpp
@@ -0,0 +1,8 @@
+#pragma comment(lib, "LLVMSupport.lib")
+
+#pragma comment(lib, "MLIRAnalysis.lib")
+#pragma comment(lib, "MLIRIR.lib")
+#pragma comment(lib, "MLIRSupport.lib")
+#pragma comment(lib, "MLIRParser.lib")
+#pragma comment(lib, "MLIRSideEffectInterfaces.lib")
+#pragma comment(lib, "MLIRTransforms.lib")
