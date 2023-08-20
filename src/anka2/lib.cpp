d43f0b242de830a3e3be1b254297e64c8302fb32
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2023-08-20 10:53:08 +0200

    Revert "add basic project with llvm and mlir connection"
    This reverts commit db40977f9e8256a4d4e595097c3099a60666e6b6.

--- src/anka2/lib.cpp
+++
@@ -1,8 +0,0 @@
-#pragma comment(lib, "LLVMSupport.lib")
-
-#pragma comment(lib, "MLIRAnalysis.lib")
-#pragma comment(lib, "MLIRIR.lib")
-#pragma comment(lib, "MLIRSupport.lib")
-#pragma comment(lib, "MLIRParser.lib")
-#pragma comment(lib, "MLIRSideEffectInterfaces.lib")
-#pragma comment(lib, "MLIRTransforms.lib")
